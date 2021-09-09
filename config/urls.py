cb6d1fd023e92daf1f6824de00b04ca525f4cfd2
madiyorbek <madiyor5552@gmail.com>
2021-09-09 11:12:33 +0500

    swagger commented, as it is not working in django version 2.2.10 in docker i commented to continue later on

--- config/urls.py
+++ config/urls.py
@@ -8,25 +8,25 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Polls API endpoints",
-        default_version='v1',
-        description="This page contains all the public and private endpoints",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="madiyor5552@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Polls API endpoints",
+#         default_version='v1',
+#         description="This page contains all the public and private endpoints",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="madiyor5552@gmail.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(AllowAny,),
+# )
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/polls/',include('polls.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
